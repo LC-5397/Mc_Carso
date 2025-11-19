dc33ddd5e9eaf9351feb0716d0eb4bb17a00a392
Emilio0212 <194541-4@iberoleon.edu.mx>
2025-11-18 22:29:56 -0600

    version 2

---
+++ modelo/incidentes.py
@@ -0,0 +1,7 @@
+class Incidentes():
+    def __init__(self):
+        self.id_incidente : int=0
+        self.id_proyecto : int=0
+        self.id_empleado : int=0
+        self.descripcion : str=""
+        self.fecha : str=""
