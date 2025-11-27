a5378a86dce31de77679670d452322aa459124d7
Emilio0212 <194541-4@iberoleon.edu.mx>
2025-11-27 14:05:11 -0600

    funcion para usuario

--- modelo/usuariodao.py
+++ modelo/usuariodao.py
@@ -15,3 +15,14 @@ class UsuarioDAO:
             datos = cursor.fetchall()
             basedatos.cerrarConexionBD()
             return datos
+    
+    def validarLogin(self, username, password):
+        basedatos = ConexionBD()
+        basedatos.establecerConexionBD()
+        # Llamamos al procedimiento almacenado
+        sql = "EXEC dbo.sp_login_info_completa @username = ?, @password = ?"
+        cursor = basedatos.conexion.cursor()
+        cursor.execute(sql, (username, password))
+        datos = cursor.fetchone() 
+        basedatos.cerrarConexionBD()
+        return datos # Retorna la info del empleado o None si falló
